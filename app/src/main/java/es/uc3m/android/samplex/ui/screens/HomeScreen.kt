740b1152ef07d18175e2c2e788ee4244fad55371
Alex-Taboada <alejandro.taboada@missioia.com>
2025-05-07 12:04:49 +0200

    arreglar error con el icono del fuego

--- app/src/main/java/es/uc3m/android/samplex/ui/screens/HomeScreen.kt
+++ app/src/main/java/es/uc3m/android/samplex/ui/screens/HomeScreen.kt
@@ -27,7 +27,7 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.LocalFireDepartment
+import androidx.compose.material.icons.filled.Whatshot
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -470,7 +470,7 @@ fun HomeScreen(
                                 verticalAlignment = Alignment.CenterVertically
                             ) {
                                 Icon(
-                                    imageVector = Icons.Default.LocalFireDepartment,
+                                    imageVector = Icons.Default.Whatshot,
                                     contentDescription = null,
                                     tint = BabyBlueDark,
                                     modifier = Modifier.size(48.dp)
